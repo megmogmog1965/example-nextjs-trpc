ce7633e3ba323d4ed73006a7b5827333ac4524a3
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2024-10-30 19:55:14 +0900

    css.

--- components/greeting-client.tsx
+++ components/greeting-client.tsx
@@ -7,7 +7,7 @@ export function GreetingClient({ name }: { name: string }) {
   const greeting = trpc.hello.useQuery({ text: name })
 
   return (
-    <div className="bg-red-100 p-5 border-2 border-red-500">
+    <div className="bg-red-100 border-2 border-red-500 rounded-md m-2 p-5 space-y-2">
       <div className="text-red-500 font-bold">Client Component</div>
       <div>{JSON.stringify(greeting.data)}</div>
     </div>
--- components/greeting-server.tsx
+++ components/greeting-server.tsx
@@ -5,8 +5,8 @@ export async function GreetingServer({ name }: { name: string }) {
   const greeting = await trpc.hello({ text: name })
 
   return (
-    <div className="bg-red-100 p-5 border-2 border-red-500">
-      <div className="text-red-500 font-bold">Server Component</div>
+    <div className="bg-blue-100 border-2 border-blue-500 rounded-md m-2 p-5 space-y-1">
+      <div className="text-blue-500 font-bold">Server Component</div>
       <div>{JSON.stringify(greeting)}</div>
     </div>
   )
